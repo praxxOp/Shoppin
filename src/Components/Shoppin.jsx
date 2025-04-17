9665dbc0eeb6ca45b28265a1c5ad5413633a4ecc
Prathamesh <shelke.prathamesh0012@gmail.com>
2025-04-17 23:03:39 +0530

    modified:   src/Components/Shoppin.css 	modified:   src/Components/Shoppin.jsx 	new file:   src/Fonts/ZENTRY-Regular.ttf 	new file:   src/Fonts/neuo.otf 	new file:   src/assets/CartM.svg 	new file:   src/assets/HeartM.svg 	modified:   src/index.css

--- src/Components/Shoppin.jsx
+++ src/Components/Shoppin.jsx
@@ -4,31 +4,47 @@ import Data from "../Data";
 import Like from "./Shoppin-Components/LikeComponent/Like";
 import AddToCart from "./Shoppin-Components/AddToCart/AddToCart";
 import logo from "../assets/logo.webp";
+import LikeImg from "../assets/HeartM.svg";
+import CartImg from "../assets/CartM.svg";
+
 const Shoppin = () => {
+  const product = Data[0];
+
+  if (!product) {
+    return <div>Loading product...</div>;
+  }
+
   return (
-    <div className="Shoppin">
-      <div className="Shoppin-container">
-        <div className="Shoppin-Navbar">
-          <div className="Shoppin-Navbar-left">
-            <Like />
+    <div className="shoppin-page">
+      <div className="shoppin-header">
+        <img src={LikeImg} alt="Like" className="header-icon" />
+        <img src={logo} alt="Shoppin Logo" className="header-logo" />
+        <img src={CartImg} alt="Cart" className="header-icon" />
+      </div>
+
+      <div className="product-card-container">
+        <div className="product-card">
+          <div className="product-image-container">
+             <img src={product.imageUrl} alt={product.name} className="product-image"/>
           </div>
-          <div className="Shoppin-Navbar-logo">
-            <img src={logo} alt="logo" />
-          </div>
-          <div className="Shoppin-Navbar-right">
-            <AddToCart />
-          </div>
-        </div>
-        <div className="Shoppin-products">
-          {Data.map((item) => (
-            <div className="Shoppin-product-card" key={item.id}>
-              <img src={item.imageUrl} alt={item.name} />
-              <h3>{item.name}</h3>
-              <p>{item.brand}</p>
-              <p>{item.price}</p>
+          <div className="product-details">
+            <div className="product-info">
+              <h2 className="product-name">{product.name}</h2>
+              <p className="product-brand">
+                Brand: <a href="#" className="brand-link">{product.brand}</a>
+              </p>
             </div>
-          ))}
+            <div className="product-pricing">
+              <span className="original-price">Rs. {product.originalPrice}</span>
+              <span className="current-price">Rs. {product.price}</span>
+              <span className="discount">({product.discountPercentage}% OFF)</span>
+            </div>
+          </div>
         </div>
+      </div>
+
+      <div className="footer-text">
+        SHOPPIN'
       </div>
     </div>
   );
