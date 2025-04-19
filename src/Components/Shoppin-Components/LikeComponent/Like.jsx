018110efd86b74d867132564f05de1f8f6b5030a
Prathamesh <shelke.prathamesh0012@gmail.com>
2025-04-19 16:36:55 +0530

    modified:   src/Components/Shoppin-Components/AddToCart/AddToCart.css 	modified:   src/Components/Shoppin-Components/LikeComponent/Like.css 	modified:   src/Components/Shoppin-Components/LikeComponent/Like.jsx

--- src/Components/Shoppin-Components/LikeComponent/Like.jsx
+++ src/Components/Shoppin-Components/LikeComponent/Like.jsx
@@ -3,17 +3,24 @@ import "./Like.css";
 const Like = ({LikeP}) => {
   const Products = LikeP;
   return (
-    <>
-      <div className="Like-main">
-        {Products.map((Items, Id) => {
-          return (
-            <div key={Id} className="Like-product-Component">
-              {Items.name}
+    <div className="Like-main">
+      {Products.length === 0 ? (
+        <div className="empty-like-message">No liked products yet.</div>
+      ) : (
+        Products.map((Items, Id) => (
+          <div key={Id} className="Like-product-Component">
+            <div className="like-left">
+              <div className="like-name">{Items.name}</div>
+              <div className="like-brand">{Items.brand}</div>
+              <div className="like-price">Rs.{Items.price}</div>
             </div>
-          );
-        })}
-      </div>
-    </>
+            <div className="like-right">
+              <img src={Items.imageUrl} className="like-img" alt={Items.name} />
+            </div>
+          </div>
+        ))
+      )}
+    </div>
   );
 };
 
